a4caf32252b13c86db17a1b52e98247c84d266e8
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-10-10 17:05:44 +0200

    feat(bazarliste): create options to select intra filter mode

---
+++ lang/twolevels_fr.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the YesWiki Extension twolevels.
+ *
+ * Authors : see README.md file that was distributed with this source code.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+return [
+    // services/ActionsBuilderService.php
+    'TWOLEVELS_INTRAFILTERSMODE_LABEL' => 'Comportement des filtres d\'une même facette',
+    'TWOLEVELS_INTRAFILTERSMODE_HINT' => 'ATTENTION : n\'est pris en compte que pour `dynamic=true` ou `bazarcard`',
+    'TWOLEVELS_INTRAFILTERSMODE_OR' => 'ou (par défaut)',
+    'TWOLEVELS_INTRAFILTERSMODE_AND' => 'et',
+];
